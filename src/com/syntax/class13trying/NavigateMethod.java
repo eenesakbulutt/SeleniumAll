a7390db652f857ec066d007c9cae9ac1cbd5fc0e
Enes Akbulut <eenesakbulutt@gmail.com>
2021-10-19 12:22:36 -0400

    class13

---
+++ src/com/syntax/class13trying/NavigateMethod.java
@@ -0,0 +1,39 @@
+package com.syntax.class13trying;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class NavigateMethod {
+    public static void main(String[] args)throws InterruptedException {
+
+System.setProperty("webdriver.chrome.driver","Drivers/chromedriver");
+
+WebDriver driver=new ChromeDriver();
+
+driver.get("https://www.facebook.com/");
+
+driver.navigate().to("https://www.google.com/");
+
+driver.navigate().back();
+
+driver.navigate().forward();
+
+Thread.sleep(2000);
+
+driver.navigate().refresh();
+
+// driver.close(); //will close current tab.
+
+driver.quit(); // will quit the whole browser.
+
+
+
+
+//get method bunlari yapamiyor
+        //navigate ileri geri ielrleyebiliyoruz rahat bir sekilde.
+
+
+
+
+    }
+}
